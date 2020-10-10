759ea42391eb667781fcd8c16fc58926bbf920e3
TaylorAndy <53394624+liuan0803@users.noreply.github.com>
2020-10-10 16:29:38 +0800

    Update checkdata.py

--- checkdata.py
+++ checkdata.py
@@ -1,22 +1,6 @@
 # -*- coding:utf-8 -*-
-import csv
-import os
-import glob
-import sys
-import shutil
-import os
-import cv2
+import csv,os,glob,sys,shutil,cv2
 import xml.etree.ElementTree as ET
-
-"""
-目的：将原图片(img)与其xml(xml)，合成为打标记的图片(labelled)，矩形框标记用红色即可
-已有：（1）原图片文件夹(imgs_path)，（2）xml文件夹(xmls_path)
-思路：
-    step1: 读取（原图片文件夹中的）一张图片
-    step2: 读取（xmls_path）该图片的xml文件，并获取其矩形框的两个对角顶点的位置
-    step3: 依据矩形框顶点坐标，在该图片中画出该矩形框
-    step4: 图片另存为'原文件名'+'_labelled'，存在‘lablled’文件夹中
-"""
 
 def xml_jpg2labelled(imgs_path, xmls_path, labelled_path):
     '''
@@ -46,15 +30,9 @@ def xml_jpg2labelled(imgs_path, xmls_path, labelled_path):
             labelled = cv2.rectangle(labelled, (xmin, ymin), (xmax, ymax), (0, 0, 255), 1)
             cv2.putText(labelled, name, (xmin, max(ymin - 2, 0)), cv2.FONT_HERSHEY_COMPLEX, 0.75, (0, 255, 0), 1)
         cv2.imwrite('%slabelled_%s' % (labelled_path + '/', imgs_list[i]), labelled)
-        # cv.imshow('labelled', labelled)
-        # cv.imshow('origin', origin)
-        # cv.waitKey()
 
 
 if __name__ == '__main__':
-    # imgs_path = r'F:\post_graduate_design\infared_plane_data\traindata_new\orgdata'
-    # xmls_path = r'F:\post_graduate_design\infared_plane_data\traindata_new\orgxml'
-    # labelled_path = r'F:\post_graduate_design\postgraduate_continue\check_data_and_sign\check_bbox_on_img\traindata'
 
     imgs_path = r'F:\post_graduate_design\infared_plane_data\traindata_new\output\jpgout'
     xmls_path = r'F:\post_graduate_design\infared_plane_data\traindata_new\output\xmlout'
